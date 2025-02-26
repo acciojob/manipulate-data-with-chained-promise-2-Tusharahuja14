24f21e0bc47f445aea7e6e595784f32f6218fab5
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-02-26 23:58:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 let arr = [1, 2, 3, 4];
 let output = document.getElementById("output");
 
-output.innerText = ""; 
-
 // Step 1: Filter out odd numbers
 let promise1 = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
             arr = arr.filter((elem) => elem % 2 === 0);
-            output.innerText = arr.join(", "); // Display even numbers
+            output.innerText = arr.join(", "); // Display the filtered array
             resolve(); 
-        }, 1000); // 1-second delay
+        }, 1000); 
     });
 };
 
@@ -19,9 +17,9 @@ let promise2 = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
             arr = arr.map((elem) => elem * 2);
-            output.innerText = arr.join(", "); // Display multiplied numbers
+            output.innerText = arr.join(", "); // Display the modified array
             resolve(); 
-        }, 2000); // 2-second delay
+        }, 2000); 
     });
 };
 
