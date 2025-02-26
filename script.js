4d650f91ad65107d58101af882aae16f3e6f2588
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-02-26 23:32:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
 let arr = [1, 2, 3, 4];
 let output = document.getElementById("output");
 
-let initialPromise = () => {
+let promise1 = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve(arr);
-        }, 0); // Start immediately, no need for 3s delay
-    });
-};
-
-let filterEvenNumbers = (arr) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            arr = arr.filter(num => num % 2 === 0);
-            output.innerText = arr.join(", ");
+            arr = arr.filter((elem) => elem % 2 === 0);
+            output.innerText = arr.join(", "); 
             resolve(arr);
         }, 1000);
     });
 };
 
-let multiplyByTwo = (arr) => {
+let promise2 = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            arr = arr.map(num => num * 2);
-            output.innerText = arr.join(", ");
+            arr = arr.map((elem) => elem * 2);
+            output.innerText = arr.join(", "); // Update UI
             resolve(arr);
         }, 2000);
     });
 };
 
-initialPromise()
-    .then(filterEvenNumbers)
-    .then(multiplyByTwo)
-    .catch(err => console.error(err));
+promise1()
+    .then(() => promise2())
+    .catch((err) => console.error(err));
